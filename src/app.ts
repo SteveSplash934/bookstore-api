b69582fdf43e141264fe2040c2f0449f77d0e2e2
Steve Splash <stevesplash4@gmail.com>
2025-01-07 13:56:12 -0800

    views now working after configured with 'copyfiles'

--- src/app.ts
+++ src/app.ts
@@ -2,43 +2,45 @@ import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import * as dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import Logger from './middlewares/Logger';
-import userRoutes from './routes/userRoutes';
-import bookRoutes from './routes/bookRoutes';
-import appRoutes from './routes/appRoutes';
+// import mongoose from 'mongoose';
+import Logger from './middlewares/Logger.js';
+import userRoutes from './routes/userRoutes.js';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import bookRoutes from './routes/bookRoutes.js';
+import appRoutes from './routes/appRoutes.js';
 
-dotenv.config({ path: '.env' });
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-const HOSTNAME = process.env.HOSTNAME || 'localhost';
-const PORT = parseInt(process.env.PORT || '3000');
+
 
 // Application setup
+dotenv.config({ path: '.env' });
 const app = express();
-
-// Set the 'public' folder as both the views folder and static assets folder
 app.set('view engine', 'ejs');
-app.set('views', 'public');  // Set views to 'public' folder
-
-// Serve static files (CSS, JS, images) from the 'public' folder
-app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
 
 // Middleware setup
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+const HOSTNAME = process.env.HOSTNAME || 'localhost';
+const PORT = parseInt(process.env.PORT || '3000');
 
 // Logger middleware
 app.use((req, res, next) => {
-    Logger.log_client_request(req, res, next); // Pass next
-});
+    Logger.log_client_request(req, res, next);
+  });
 
 // Routes
 app.use('/user', userRoutes);
 app.use('/api/v1', bookRoutes);
 app.use('/app', appRoutes);
 
-// Redirect root to /app
 app.get('/', (req, res) => {
   res.redirect('/app');
 });
